f67e1c418d012e9e69678a406cf4646f7c663d0b
SPICa27 <1286939372@qq.com>
2024-12-11 23:26:38 +0800

    ## 适配小白条

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:easy_refresh/easy_refresh.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -40,9 +43,27 @@ class MyApp extends StatelessWidget {
           },
           builder: EasyLoading.init(),
           // 主题
-          theme: const MaterialTheme(TextTheme()).light(),
+          theme: const MaterialTheme(TextTheme()).light().copyWith(
+            appBarTheme: const AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent,
+                statusBarIconBrightness: Brightness.dark,
+                systemNavigationBarColor: Colors.white,
+                systemNavigationBarIconBrightness: Brightness.dark,
+              ),
+            ),
+          ),
           // 暗黑模式
-          darkTheme: const MaterialTheme(TextTheme()).dark(),
+          darkTheme: const MaterialTheme(TextTheme()).dark().copyWith(
+            appBarTheme: const AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent,
+                statusBarIconBrightness: Brightness.light,
+                systemNavigationBarColor: Colors.black,
+                systemNavigationBarIconBrightness: Brightness.light,
+              ),
+            ),
+          ),
           // 入口
           initialRoute: AppPages.INITIAL,
           // 路由
